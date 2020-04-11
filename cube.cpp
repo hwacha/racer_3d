aa01830a8929890af71b6fff8c515ce88104721d
Jimmy <talyian@gmail.com>
2020-04-11 19:16:37 -0400

    add texture to cube

--- cube.cpp
+++ cube.cpp
@@ -8,52 +8,95 @@
 #include "shader.h"
 
 unsigned int make_cube() {
-    float vertices[] = {
-        1.0, 1.0, 1.0,
-        -1.0, 1.0, 1.0,
-        -1.0, -1.0, 1.0,
-        1.0, -1.0, 1.0,
-        1.0, 1.0, -1.0,
-        -1.0, 1.0, -1.0,
-        -1.0, -1.0, -1.0,
-        1.0, -1.0, -1.0,
-    };
+  float vertices[] = {
+	0, 0, 0, 0, 0, 0, 0, -1,
+	1, 0, 0, 1, 0, 0, 0, -1,
+	1, 1, 0, 1, 1, 0, 0, -1,
+	1, 1, 0, 1, 1, 0, 0, -1,
+	0, 0, 0, 0, 0, 0, 0, -1,
+	0, 1, 0, 0, 1, 0, 0, -1,
 
-    unsigned int indices[] = {
-        0,1,2,
-        0,2,3,
-        0,4,1,
-        1,4,5,
-        1,5,2,
-        2,5,6,
-        2,6,3,
-        3,6,7,
-        3,7,0,
-        0,7,4,
-        4,6,5,
-        4,7,6
-    };
+	0, 0, 1, 0, 0, 0, 0, 1,
+	1, 1, 1, 1, 1, 0, 0, 1,
+	1, 0, 1, 1, 0, 0, 0, 1,
+	1, 1, 1, 1, 1, 0, 0, 1,
+	0, 1, 1, 0, 1, 0, 0, 1,
+	0, 0, 1, 0, 0, 0, 0, 1,
 
-    unsigned int vertex_arr;
-    glGenVertexArrays(1, &vertex_arr);
-    glBindVertexArray(vertex_arr);
+	0, 0, 0, 0, 0, 0, 0, -1,
+	1, 0, 0, 1, 0, 0, 0, -1,
+	1, 1, 0, 1, 1, 0, 0, -1,
+	1, 1, 0, 1, 1, 0, 0, -1,
+	0, 0, 0, 0, 0, 0, 0, -1,
+	0, 1, 0, 0, 1, 0, 0, -1,
 
-    unsigned int vertex_buf;
-    glGenBuffers(1, &vertex_buf);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buf);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*3*8, vertices, GL_STATIC_DRAW);
+	0, 0, 0, 0, 0, 0, 0, -1,
+	1, 0, 0, 1, 0, 0, 0, -1,
+	1, 1, 0, 1, 1, 0, 0, -1,
+	1, 1, 0, 1, 1, 0, 0, -1,
+	0, 0, 0, 0, 0, 0, 0, -1,
+	0, 1, 0, 0, 1, 0, 0, -1,
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+	0, 0, 0, 0, 0, 0, 0, -1,
+	1, 0, 0, 1, 0, 0, 0, -1,
+	1, 1, 0, 1, 1, 0, 0, -1,
+	1, 1, 0, 1, 1, 0, 0, -1,
+	0, 0, 0, 0, 0, 0, 0, -1,
+	0, 1, 0, 0, 1, 0, 0, -1,
 
-    unsigned int indices_buf;
-    glGenBuffers(1, &indices_buf);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_buf);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int)*3*12, indices, GL_STATIC_DRAW);
+	0, 0, 0, 0, 0, 0, 0, -1,
+	1, 0, 0, 1, 0, 0, 0, -1,
+	1, 1, 0, 1, 1, 0, 0, -1,
+	1, 1, 0, 1, 1, 0, 0, -1,
+	0, 0, 0, 0, 0, 0, 0, -1,
+	0, 1, 0, 0, 1, 0, 0, -1,
+  };
 
-    glBindVertexArray(0);
+  for(int i = 2; i < 4; i++) {
+	  for(int j = 0; j < 6; j++) {
+	    float *src = &vertices[(i-2) * 48 + j * 8];
+	    float *dst_xz = &vertices[i * 48 + j * 8];
+      float *dst_yz = &vertices[(i + 2) * 48 + j * 8];
+	    dst_xz[0] = src[2];
+	    dst_xz[2] = src[0];
+	    dst_xz[5] = src[7];
+	    dst_xz[7] = src[5];
 
-    return vertex_arr;
+      dst_yz[1] = src[2];
+      dst_yz[2] = src[1];
+      dst_yz[6] = src[7];
+      dst_yz[7] = src[6];
+    }
+  }
+
+  unsigned int indices[6 * 6];
+  for(int i=0; i<36; i++) indices[i] = i;
+  
+  unsigned int vertex_arr;
+  glGenVertexArrays(1, &vertex_arr);
+  glBindVertexArray(vertex_arr);
+
+  unsigned int vertex_buf;
+  glGenBuffers(1, &vertex_buf);
+  glBindBuffer(GL_ARRAY_BUFFER, vertex_buf);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(5 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+  
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(2);
+
+  unsigned int indices_buf;
+  glGenBuffers(1, &indices_buf);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_buf);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glBindVertexArray(0);
+  return vertex_arr;
 }
 
 void draw_cube(
@@ -63,7 +106,7 @@ void draw_cube(
 {
     shader.setMat4("model", transform);
 
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
     glBindVertexArray(cube_va);
     glDrawElements(GL_TRIANGLES, 3*12, GL_UNSIGNED_INT, (void*)0);
