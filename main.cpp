2714326e05934505e61a998bd06a0a29d2b5e500
Alex Chen <awchen@ucalgary.ca>
2020-04-11 01:04:34 -0400

    Draw an icosahedron
    It's almost a sphere?

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <iostream>
+#include <vector>
+#include <tuple>
 
 #include "shader.h"
 
@@ -10,6 +16,69 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 const unsigned int SCR_WIDTH = 1280;
 const unsigned int SCR_HEIGHT = 720;
+
+std::vector<float> icosahedron_vertices(float radius) {
+    int i;
+    float elev_angle = atan(0.5);
+    float rot_angle = 72.0 * 3.1415 / 180.0;
+
+    std::vector<float> vertices(12*3);
+    // top
+    vertices[0*3 + 0] = 0.0;
+    vertices[0*3 + 1] = 0.0;
+    vertices[0*3 + 2] = radius;
+    // upper ring
+    for ( i = 0 ; i < 5 ; i++ ) {
+        vertices[(i+1)*3 + 0] = radius * cos(i*rot_angle) * cos(elev_angle);
+        vertices[(i+1)*3 + 1] = radius * sin(i*rot_angle) * cos(elev_angle);
+        vertices[(i+1)*3 + 2] = radius * sin(elev_angle);
+    }
+    // lower ring
+    for ( i = 0 ; i < 5 ; i++ ) {
+        vertices[(i+6)*3 + 0] = radius * cos((i+0.5)*rot_angle) * cos(elev_angle);
+        vertices[(i+6)*3 + 1] = radius * sin((i+0.5)*rot_angle) * cos(elev_angle);
+        vertices[(i+6)*3 + 2] = radius * -sin(elev_angle);
+    }
+    // bottom
+    vertices[11*3 + 0] = 0.0;
+    vertices[11*3 + 1] = 0.0;
+    vertices[11*3 + 2] = -radius;
+
+    return vertices;
+}
+
+std::vector<unsigned int> icosahedron_indices() {
+    int i;
+    unsigned int upper_ring[] = {1, 2, 3, 4, 5};
+    unsigned int lower_ring[] = {6, 7, 8, 9, 10};
+    std::vector<unsigned int> indices(20*3);
+    // row 1
+    for ( i = 0 ; i < 5 ; i++ ) {
+        indices[i*3 + 0] = 0;
+        indices[i*3 + 1] = upper_ring[i];
+        indices[i*3 + 2] = upper_ring[(i+1)%5];
+    }
+    // row 2
+    for ( i = 0 ; i < 5 ; i++ ) {
+        indices[(i+5)*3 + 0] = i+6;
+        indices[(i+5)*3 + 1] = upper_ring[i];
+        indices[(i+5)*3 + 2] = upper_ring[(i+1)%5];
+    }
+    // row 3
+    for ( i = 0 ; i < 5 ; i++ ) {
+        indices[(i+10)*3 + 0] = lower_ring[i];
+        indices[(i+10)*3 + 1] = lower_ring[(i+1)%5];
+        indices[(i+10)*3 + 2] = 1 + ((i+1)%5);
+    }
+    // row 4
+    for ( i = 0 ; i < 5 ; i++ ) {
+        indices[(i+15)*3 + 0] = lower_ring[i];
+        indices[(i+15)*3 + 1] = lower_ring[(i+1)%5];
+        indices[(i+15)*3 + 2] = 11;
+    }
+
+    return indices;
+}
 
 int main()
 {
@@ -45,15 +114,10 @@ int main()
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    float vertices[] = {
-        -0.5f, -0.5f,  0.0f,
-         0.5f, -0.5f,  0.0f,
-         0.0f,  0.5f,  0.0f,
-    };
+    std::vector<float> vertices = icosahedron_vertices(0.5);
+    std::vector<unsigned int> indices = icosahedron_indices();
 
-    unsigned int indices[] = {
-        0, 1, 2
-    };
+    /* float* vertices = std::get<0> */
 
     unsigned int vertex_arr;
     glGenVertexArrays(1, &vertex_arr);
@@ -62,7 +126,7 @@ int main()
     unsigned int vertex_buf;
     glGenBuffers(1, &vertex_buf);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buf);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*3*12, vertices.data(), GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -70,11 +134,13 @@ int main()
     unsigned int indices_buf;
     glGenBuffers(1, &indices_buf);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_buf);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int)*3*20, indices.data(), GL_STATIC_DRAW);
 
     glBindVertexArray(0);
 
     Shader shader("shaders/basic.vs", "shaders/basic.fs");
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     while(!glfwWindowShouldClose(window)) {
     	// handle inputs
@@ -87,8 +153,19 @@ int main()
 
       shader.use();
 
+      glm::mat4 view;
+      view = glm::lookAt(
+          glm::vec3(0.0f, 0.0f, 3.0f),
+          glm::vec3(0.0f, 0.0f, 0.0f),
+          glm::vec3(0.0f, 1.0f, 0.0f));
+      shader.setMat4("view", view);
+
+      glm::mat4 projection;
+      projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+      shader.setMat4("projection", projection);
+
       glBindVertexArray(vertex_arr);
-      glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*)0);
+      glDrawElements(GL_TRIANGLES, 3*20, GL_UNSIGNED_INT, (void*)0);
       glBindVertexArray(0);
 
     	// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -106,7 +183,7 @@ int main()
 void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true); 
+        glfwSetWindowShouldClose(window, true);
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
