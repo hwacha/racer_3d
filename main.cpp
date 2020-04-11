d1887670dcda2555d51b16481b1a98557c3eefb7
Alex Chen <awchen@ucalgary.ca>
2020-04-10 20:52:14 -0400

    Draw a triangle

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 
+#include "shader.h"
+
 void processInput(GLFWwindow *window);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
@@ -24,7 +26,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Racer3D", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -43,19 +45,56 @@ int main()
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    float vertices[] = {
+        -0.5f, -0.5f,  0.0f,
+         0.5f, -0.5f,  0.0f,
+         0.0f,  0.5f,  0.0f,
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2
+    };
+
+    unsigned int vertex_arr;
+    glGenVertexArrays(1, &vertex_arr);
+    glBindVertexArray(vertex_arr);
+
+    unsigned int vertex_buf;
+    glGenBuffers(1, &vertex_buf);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buf);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    unsigned int indices_buf;
+    glGenBuffers(1, &indices_buf);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_buf);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glBindVertexArray(0);
+
+    Shader shader("shaders/basic.vs", "shaders/basic.fs");
+
     while(!glfwWindowShouldClose(window)) {
     	// handle inputs
     	processInput(window);
 
     	// render
-        // ------
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+      // ------
+      glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+      shader.use();
+
+      glBindVertexArray(vertex_arr);
+      glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*)0);
+      glBindVertexArray(0);
 
     	// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+      // -------------------------------------------------------------------------------
+      glfwSwapBuffers(window);
+      glfwPollEvents();
     }
 
     glfwTerminate();
